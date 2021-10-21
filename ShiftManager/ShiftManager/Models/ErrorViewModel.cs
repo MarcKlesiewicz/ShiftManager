0451f54d42b4f1019474c0f5b6e9dd2f9f6453e7
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2021-10-21 19:19:49 +0200

    Initial commit

---
+++ ShiftManager/ShiftManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ShiftManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
